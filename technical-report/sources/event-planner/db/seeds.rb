d21abeb9ee41476ac8be8c25c8edc74ad84358bd
Raphael Lopes Feitoza <raphael.lopesfeitoza@shopify.com>
2022-07-08 14:45:34 -0700

    Seed database from fixtures

--- technical-report/sources/event-planner/db/seeds.rb
+++ technical-report/sources/event-planner/db/seeds.rb
@@ -6,3 +6,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#Seeds database from fixtures.
+Rake.application['db:fixtures:load'].invoke
