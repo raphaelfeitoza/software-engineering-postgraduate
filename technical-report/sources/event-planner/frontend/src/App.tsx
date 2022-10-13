6cd97970721c13892673cfa04574a4c0b310ad62
Raphael Lopes Feitoza <raphael.lopesfeitoza@shopify.com>
2022-10-13 11:22:25 -0300

    fix welcome pages

--- technical-report/sources/event-planner/frontend/src/App.tsx
+++ technical-report/sources/event-planner/frontend/src/App.tsx
@@ -14,17 +14,6 @@ function App() {
         <Route path="events" element={<EventListPage />} />
         <Route path="new-event" element={<CreateEventPage />} />
       </Routes>
-      <div className="col-lg-6 mx-auto px-4 py-5 my-5 text-center">
-        <p className="lead mb-4">
-          Bem vindo ao sistema EventPlanner.
-
-          <br/>
-          <br/>
-          Prova de conceito criada como parte do Projeto Integrado do curso Arquitetura de Software Distribuído
-          Puc Minas Virtual
-        </p>
-
-      </div>
     </>
   );
 }
--- technical-report/sources/event-planner/frontend/src/pages/HomePage.tsx
+++ technical-report/sources/event-planner/frontend/src/pages/HomePage.tsx
@@ -5,6 +5,16 @@ export function HomePage() {
   return (
     <div className="App">
       <Menu />
+      <div className="col-lg-6 mx-auto px-4 py-5 my-5 text-center">
+        <p className="lead mb-4">
+          Bem vindo ao sistema EventPlanner.
+          <br />
+          <br />
+          Prova de conceito criada como parte do Projeto Integrado do curso Arquitetura de Software Distribuído
+          Puc Minas Virtual
+        </p>
+
+      </div>
     </div>
   );
 }
