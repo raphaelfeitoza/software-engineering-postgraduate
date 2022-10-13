0cb57e0ae2f58fe90b9b004c659f3f7d8dbe2d30
Raphael Lopes Feitoza <raphael.lopesfeitoza@shopify.com>
2022-10-13 09:59:25 -0300

    Update start page

--- technical-report/sources/event-planner/frontend/src/App.tsx
+++ technical-report/sources/event-planner/frontend/src/App.tsx
@@ -7,13 +7,25 @@ import { CreateEventPage } from './pages/CreateEventPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="events/:eventId" element={<ScheduleUsersPage />} />
-      <Route path="events" element={<EventListPage />} />
-      <Route path="new-event" element={<CreateEventPage />} />
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="events/:eventId" element={<ScheduleUsersPage />} />
+        <Route path="events" element={<EventListPage />} />
+        <Route path="new-event" element={<CreateEventPage />} />
+      </Routes>
+      <div className="col-lg-6 mx-auto px-4 py-5 my-5 text-center">
+        <p className="lead mb-4">
+          Bem vindo ao sistema EventPlanner.
 
-    </Routes>
+          <br/>
+          <br/>
+          Prova de conceito criada como parte do Projeto Integrado do curso Arquitetura de Software Distribuído
+          Puc Minas Virtual
+        </p>
+
+      </div>
+    </>
   );
 }
 
